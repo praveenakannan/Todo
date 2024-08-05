dfb7c6f5aa3b426da005351ba35a5bf93d33fa3c
praveenakannan <praveenakannan.p@gmail.com>
2024-08-05 21:20:59 +0530

    Update Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,79 +3,77 @@ import React, { useState } from 'react';
 
 const Todo = () => {
    
-//    const [addValue,setAddValue] = useState("");
-//    const [data,setData]=useState([]);
-   
-
-//    const handleAdd = ()=>{
-//     if(!addValue){
-//         alert("Enter the Name and Description");
-//     }
-//     else{
+   const [addValue,setAddValue] = useState("");
+   const [data,setData]=useState([]);
+     
+   const handleAdd = ()=>{
+    if(!addValue){
+        alert("Enter the Name and Description");
+    }
+    else{
        
-//         setData([...data,addValue]);
-//         setAddValue("");
-//     }
-//         }
-// const handleRemove = (index)=>{
-//     const remove = data.filter((item,indexItem)=>indexItem !==index);
-//     setData(remove);
-// }
-// const handleEdit = (index)=>{
-//     let newEditItem=item.find((index)=>{
-//         return index.data
-//     })
-// }
+        setData([...data,addValue]);
+        setAddValue("");
+    }
+        }
+const handleRemove = (index)=>{
+    const remove = data.filter((item,indexItem)=>indexItem !==index);
+    setData(remove);
+}
+const handleEdit = (index)=>{
+    let newEditItem=item.find((index)=>{
+        return index.data
+    })
+}
 
-//    return (
-// <div className='todolist'>
+   return (
+<div className='todolist'>
             
-//       <h1>My todo</h1>
+      <h1>My todo</h1>
 
-//     <div className="todo-wrapper">
-//         <div className="todo-input">
-//             <div className="d-flex gap-2 align-item-center">
-//                 <input type="text" className='name' placeholder='Todo Name' onChange={(e)=>setAddValue(e.target.value)} value={addValue} />          
-//                 <input type="text" className='description' placeholder='Todo Description' onChange={(e)=>setAddValue(e.target.value)} value={addValue} />         
-//                 <button type='button' className='primaryBtn' onClick={()=>handleAdd()} >Add Todo</button>
-//             </div>
+    <div className="todo-wrapper">
+        <div className="todo-input">
+            <div className="d-flex gap-2 align-item-center">
+                 <input type="text" className='name' placeholder='Todo Name' onChange={(e)=>setAddValue(e.target.value)} value={addValue} />          
+                 <input type="text" className='description' placeholder='Todo Description' onChange={(e)=>setAddValue(e.target.value)} value={addValue} />         
+                <button type='button' className='primaryBtn' onClick={()=>handleAdd()} >Add Todo</button>
+             </div>
 
             
-//         </div>
-//     </div>
-//     <div className='drop'>My Todos</div>
-//     <div className='drop'>Status Filter</div>
+         </div>
+     </div>
+     <div className='drop'>My Todos</div>
+     <div className='drop'>Status Filter</div>
 
 
-
-// <div className='cards-grouo'>
+ <div className='cards-grouo'>
     
-//         {
-//             data.map((currItem, index)=>{
-//                 return(
-//                     <>
-//                    <div className='card'>
-//                     <p className='card-text'>
-//                     Name: {currItem} <br />
-//                     Description:{currItem}
+         {
+             data.map((currItem, index)=>{
+                 return(
+                     <>
+                    <div className='card'>
+                     <p className='card-text'>
+                     Name: {currItem} <br />
+                     Description:{currItem}
                     
-//                     </p>
-//                     <button className='remove' onClick={()=> handleRemove(index)}>Delete</button>
-//                         <button className='btn-edit' onClick={()=> handleEdit(index)}>Edit</button>
-//                    </div>
+                     </p>
+                     <button className='remove' onClick={()=> handleRemove(index)}>Delete</button>
+                         <button className='btn-edit' onClick={()=> handleEdit(index)}>Edit</button>
+                    </div>
                       
                         
                     
-//                     </>
-//                 );
-//             })
-//         }
+                     </>
+                 );
+             })
+         }
     
      
-// </div>
-// </div>
+ </div>
+ </div>
        
-//     );
+     );
 };
 
 export default Todo;
